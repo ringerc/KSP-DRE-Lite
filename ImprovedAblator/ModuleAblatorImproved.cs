2507b651bba8258a6d859c8708658023204e6acc
Craig Ringer <craig@2ndquadrant.com>
2018-06-16 22:44:22 +0800

    Basic sample

---
+++ ImprovedAblator/ModuleAblatorImproved.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace ImprovedAblator
+{
+	public class ModuleAblatorImproved : ModuleAblator
+	{
+		/// <summary>
+		/// Called when the part is started by Unity.
+		/// </summary>
+		public override void OnStart(StartState state)
+		{
+			// Add stuff to the log
+			print("Engage Ablators!");
+			base.OnStart (state);
+		}
+
+		public override void OnFixedUpdate ()
+		{
+			print ("tick");
+			Console.WriteLine ("{0} ablator remaining", this.ablativeAmount);
+			base.OnFixedUpdate ();
+		}
+	}
+}
